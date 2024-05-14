3dc500262f74c9b0ed3a1437e09bc6297eef3605
YeeunWoo <yeeun2215@naver.com>
2024-05-14 17:38:04 +0900

    2024-05-14

--- jspwork/thisisjava/src/ch01/sec11/Main.java
+++ jspwork/thisisjava/src/ch01/sec11/Main.java
@@ -1,35 +1,20 @@
 package ch01.sec11;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-        try {
-            // 세 개의 숫자를 입력받음
-            long A = scanner.nextLong();
-            long B = scanner.nextLong();
-            long C = scanner.nextLong();
-
-            // 입력 범위 확인
-            if (A < 1 || A > Math.pow(10, 12) || B < 1 || B > Math.pow(10, 12) || C < 1 || C > Math.pow(10, 12)) {
-                System.out.println("입력이 유효하지 않습니다.");
-                return; // 프로그램 종료
-            }
-
-            // A, B, C를 더하여 결과를 계산
-            long sum = A + B + C;
-
-            // 결과 출력
-            System.out.println(sum);
-
-        } catch (Exception e) {
-            System.out.println("입력이 유효하지 않습니다.");
-        } finally {
-            // 스캐너 닫기
-            scanner.close();
-        }
-    }
+		String[] arr = {"aasd","b","c","d","e"};
+		System.out.println(Arrays.toString(arr));
+		System.out.println();
+		
+		String str = "abcd";
+		char[] arr2 = str.toCharArray();
+		System.out.println(Arrays.toString(arr2));
+		String str2 = new String(arr2);
+		System.out.println(str2);
+	}
 }
-
--- jspwork/thisisjava/src/ch01/sec11/Solution.java
+++ jspwork/thisisjava/src/ch01/sec11/Solution.java
@@ -4,20 +4,53 @@ import java.util.Scanner;
 import java.io.FileInputStream;
 
 public class Solution {
-
 	public static void main(String[] args) throws Exception {
-
 		System.setIn(new FileInputStream("src/ch01/sec11/input.txt"));
+		Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();  // 테스트 케이스 수 입력 받기
+        
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int N = scanner.nextInt();  // 배열 A의 길이
+            int M = scanner.nextInt();  // 배열 B의 길이
 
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-		
-		for(int test_case = 1; test_case <= T; test_case++) {
-			int A = sc.nextInt();
-			int B = sc.nextInt();
-			System.out.println("#" + test_case + " " + (A/B) + " " + (A%B));
-			
-		}
+            int[] A = new int[N];
+            int[] B = new int[M];
+
+            for (int i = 0; i < N; i++) {
+                A[i] = scanner.nextInt();
+            }
+            for (int i = 0; i < M; i++) {
+                B[i] = scanner.nextInt();
+            }
+
+            int maxSum = 0;
+            // 길이가 짧은 배열을 움직이는 방식으로 비교
+            if (N <= M) {
+                for (int i = 0; i <= M - N; i++) {
+                    int sum = 0;
+                    for (int j = 0; j < N; j++) {
+                        sum += A[j] * B[i + j];
+                    }
+                    if (sum > maxSum) {
+                        maxSum = sum;
+                    }
+                }
+            } else {
+                for (int i = 0; i <= N - M; i++) {
+                    int sum = 0;
+                    for (int j = 0; j < M; j++) {
+                        sum += A[i + j] * B[j];
+                    }
+                    if (sum > maxSum) {
+                        maxSum = sum;
+                    }
+                }
+            }
+
+            // 결과 출력
+            System.out.println("#" + test_case + " " + maxSum);
+        }
+
+        scanner.close();
 	}
 }
